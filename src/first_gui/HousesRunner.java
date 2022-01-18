3dec8accffd360134f1ec6f4dafa8bd5ffbebfa9
akanche <76064566+akanche@users.noreply.github.com>
2022-01-18 12:00:05 -0600

    s

--- src/first_gui/HousesRunner.java
+++ src/first_gui/HousesRunner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 
 public class HousesRunner {
-	ArrayList<Houses> houses = new ArrayList<Houses>();
+	static ArrayList<Houses> houses = new ArrayList<Houses>();
 	public static void main(String[] args) {
 		
 	}
@@ -16,6 +16,21 @@ public class HousesRunner {
 		double bathsOne = inFile.nextDouble();
 		int costOne = inFile.nextInt();
 		String addressOne = inFile.nextLine();
+		String housePicOne = inFile.nextLine();
+		Houses houseOne = new Houses(sqFeetOne, bedsOne, bathsOne, costOne, addressOne, housePicOne);
+		houses.add(houseOne);
+		inFile.nextLine();
+		int sqFeetTwo = inFile.nextInt();
+		int bedsTwo = inFile.nextInt();
+		double bathsTwo = inFile.nextDouble();
+		int costTwo = inFile.nextInt();
+		String addressTwo = inFile.nextLine();
+		String housePicTwo = inFile.nextLine();
+		Houses houseTwo = new Houses(sqFeetTwo, bedsTwo, bathsTwo, costTwo, addressTwo, housePicTwo);
+		houses.add(houseTwo);
+		inFile.nextLine();
+		
+		
 	}
 	
 	public static void buildGUI() {
