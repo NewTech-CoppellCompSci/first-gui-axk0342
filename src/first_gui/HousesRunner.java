841295d627bb6e7791a0f9e60eb4c02bf6862658
akanche <76064566+akanche@users.noreply.github.com>
2022-01-18 11:20:20 -0600

    s

--- src/first_gui/HousesRunner.java
+++ src/first_gui/HousesRunner.java
@@ -1,8 +1,21 @@
 package first_gui;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 public class HousesRunner {
+	ArrayList<Houses> houses = new ArrayList<Houses>();
 	public static void main(String[] args) {
 		
+	}
+	
+	public static void makeArrayList() throws Exception{
+		Scanner inFile = new Scanner(new File ("Houses.txt"));
+		int sqFeetOne = inFile.nextInt();
+		int bedsOne = inFile.nextInt();
+		double bathsOne = inFile.nextDouble();
+		int costOne = inFile.nextInt();
+		String addressOne = inFile.nextLine();
 	}
 	
 	public static void buildGUI() {
